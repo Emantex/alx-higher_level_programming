dd102ab7240f0d40d9b5dfdeaf1d721f81a59d05
emmanuel <omanoyeemmanuel@gmail.com>
2024-01-17 01:51:37 -0800

    combination

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(0, 9):
+    for j in range(0, 9):
+        if i == 8 and j == 9:
+            print('{}{}'.format(i,j), end=' ')
+        elif i != 1 and j < 1:
+            print('{}'.format(i,j), end=' ')
